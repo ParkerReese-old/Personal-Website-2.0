31d3edeb3077d9e5d578a0c0d632ccc150620cbf
ParkerR <parker-reese@hotmail.com>
2018-03-18 13:04:57 -0600

    boiler plate! - Add theme - Add angular material to node modules - Start About Me component/section - Add picture of me and title - Add chips of character traits

---
+++ personal-website-app/src/app/about-me/about-me.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'parker-about',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss']
+})
+export class AboutMeComponent {
+  traitsList = ['Hard Working', 'Motivated', 'Team Player', 'Open Minded', 'Data Driven', 'Independant'];
+}
--- personal-website-app/src/app/app.module.ts
+++ personal-website-app/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
