70e3ce6e22e0a828d405ec94ea6831d019a2df74
ParkerR <parker-reese@hotmail.com>
2018-03-21 21:20:53 -0600

    switch between primary/accent on chip click

--- personal-website-app/src/app/about-me/about-me.component.ts
+++ personal-website-app/src/app/about-me/about-me.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'parker-about',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
   traitsList = ['Hard Working', 'Motivated', 'Team Player', 'Open Minded', 'Data Driven', 'Independant'];
+  colorMap: Map<string, string> = new Map();
+
+  ngOnInit() {
+    this.traitsList.forEach(trait => {
+      this.colorMap.set(trait, 'primary');
+    });
+  }
+
+  changeChipColor(event: Event): void {
+    const trait = event.target['innerText'];
+    this.colorMap.set(trait, this.colorMap.get(trait) === 'primary' ? 'accent' : 'primary');
+  }
+
 }
