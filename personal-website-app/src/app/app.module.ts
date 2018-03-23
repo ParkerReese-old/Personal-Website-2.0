fd59d2232e0a470c9c40f361e9bc80cfe0f2e37e
ParkerR <parker-reese@hotmail.com>
2018-03-22 20:39:55 -0600

    better styling. Add a tab header. Gotta get it centered yet

--- personal-website-app/src/app/app.module.ts
+++ personal-website-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTabsModule } from '@angular/material/tabs';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,9 @@ import { AboutMeComponent } from './about-me/about-me.component';
   imports: [
     BrowserModule,
     MatCardModule,
-    MatChipsModule
+    MatChipsModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
